0b4c41429274b9e3759673b9f9253ef24a6887aa
İbrahim EDİZ <ibrahim.ediz@gazi.edu.tr>
2022-04-12 14:34:16 +0300

    12042022_3

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,78 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const Benimkartim());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class Benimkartim extends StatelessWidget {
+  const Benimkartim({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('ilk Ekran'),
-          backgroundColor: Colors.blueGrey[800],
-        ),
-        body: Container(
-          color: Colors.blueGrey[900],
-          child: Center(
-            child: Image(
-              image: AssetImage('images/resim2222.jpg'),
+          backgroundColor: Colors.blueGrey,
+          body: SafeArea(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CircleAvatar(
+                  radius: 40,
+                  backgroundColor: Colors.blueGrey.shade900,
+                  backgroundImage: AssetImage("images/g20white.png"),
+                ),
+                Text("İbrahim EDİZ"),
+                Text("Flutter Developer"),
+                SizedBox(
+                  height: 20,
+                  width: 150,
+                  child: Divider(
+                    color: Colors.blueGrey[900],
+                  ),
+                ),
+                Card(
+                  color: Colors.white,
+                  margin:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: ListTile(
+                    leading:
+                        Icon(Icons.phone, color: Colors.blueGrey, size: 30.0),
+                    title: Center(
+                      child: Text(
+                        "+90 532 654 1234",
+                        style: TextStyle(
+                            fontSize: 20.0,
+                            color: Colors.blueGrey[900],
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    subtitle: Text("Telefon Numarası"),
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Card(
+                  color: Colors.white,
+                  margin:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: ListTile(
+                    leading:
+                        Icon(Icons.phone, color: Colors.blueGrey, size: 30.0),
+                    title: Center(
+                      child: Text(
+                        "edizdesign@gmail.com",
+                        style: TextStyle(
+                            fontSize: 20.0,
+                            color: Colors.blueGrey[900],
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
-          ),
-        ),
-      ),
+          )),
     );
   }
 }
